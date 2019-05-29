3d763636413054a77ac7c3d398a99da3e26acfce
Jonathan Raxa <jonathanraxa@gmail.com>
2019-05-29 09:27:19 -0700

    Updates

---
+++ precache-manifest.68de69b11be7dfbefcab8e770749c2e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09151323f049d977b9cb5437e3f9a2a",
+    "url": "/VidEduIO/index.html"
+  },
+  {
+    "revision": "acf84e4fd287c000f1fa",
+    "url": "/VidEduIO/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "7e75ba2c7d4fff80bc0b",
+    "url": "/VidEduIO/static/css/main.0dd06d57.chunk.css"
+  },
+  {
+    "revision": "acf84e4fd287c000f1fa",
+    "url": "/VidEduIO/static/js/2.a21f78fa.chunk.js"
+  },
+  {
+    "revision": "7e75ba2c7d4fff80bc0b",
+    "url": "/VidEduIO/static/js/main.39a328ee.chunk.js"
+  },
+  {
+    "revision": "6478491f7b82df0be625",
+    "url": "/VidEduIO/static/js/runtime~main.9932d643.js"
+  }
+]);
